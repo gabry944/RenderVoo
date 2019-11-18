75669b2a77e8f4f1f4ae5a09496188b7eedfe6a6
Gabriella Rydenfors <gabry944@student.liu.se>
2019-11-18 21:42:21 +0100

    Added SDL library and open a empty window

--- Render/Voo/main.cpp
+++ Render/Voo/main.cpp
@@ -1,7 +1,29 @@
+#include <SDL.h>
 #include <iostream>
+#include <Windows.h>
 
-int main() {
-
+int main(int argc, char* argv[]) {
   printf("Hello, World!");
+
+  SDL_Window* window = nullptr;
+
+  SDL_Surface* screen = nullptr;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not init: %s", SDL_GetError());
+    goto exit_1;
+  }
+
+  window = SDL_CreateWindow("RenderVooDoo", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, 1024, 768,
+                            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+  Sleep(2000);
+
+  printf("Goodbye, World!");
+
   return 0;
+
+exit_1:
+  return -1;
 }
