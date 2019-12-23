34df77df59833f84ad05eea15b7ec7947dab521b
Mik-pe <mikael.pettersson201@gmail.com>
2019-12-23 21:23:59 +0100

    Don't use goto for error

--- Render/Voo/main.cpp
+++ Render/Voo/main.cpp
@@ -2,18 +2,21 @@
 #include <iostream>
 #include <Windows.h>
 
+
+
 int main(int argc, char* argv[]) {
   printf("Hello, World!");
-
+  
   SDL_Window* window = nullptr;
 
   SDL_Surface* screen = nullptr;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not init: %s", SDL_GetError());
-    goto exit_1;
-  }
+    return -1;
+  } 
 
+ 
   window = SDL_CreateWindow("RenderVooDoo", SDL_WINDOWPOS_UNDEFINED,
                             SDL_WINDOWPOS_UNDEFINED, 1024, 768,
                             SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
@@ -24,6 +27,5 @@ int main(int argc, char* argv[]) {
 
   return 0;
 
-exit_1:
-  return -1;
+  
 }
